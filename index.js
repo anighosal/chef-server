9b59fe80838b87b509dc9408f842e19745916612
anighosal <121676026+anighosal@users.noreply.github.com>
2023-05-02 03:05:40 +0600

    allchef json data added

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const allchef = require("./Data/allchef.json");
+
 app.get("/", (req, res) => {
   res.send("chef hunter is running");
 });
 
+app.get("/allchef", (req, res) => {
+  res.send(allchef);
+});
+
 app.listen(port, () => {
-  console.log(`Dragon API is running on port:${port}`);
+  console.log(`chef API is running on port:${port}`);
 });
