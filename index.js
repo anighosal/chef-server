36dca403d5b02e64fc2dd95014b80df3a108940d
anighosal <121676026+anighosal@users.noreply.github.com>
2023-05-03 14:51:12 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const allchef = require("./Data/allchef.json");
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("chef hunter is running");
@@ -11,7 +13,12 @@ app.get("/", (req, res) => {
 app.get("/allchef", (req, res) => {
   res.send(allchef);
 });
+app.get("/allchef/:id", (req, res) => {
+  const id = req.params.id;
+  const find = allchef.find((chef) => chef.id == id);
 
+  res.send(find);
+});
 app.listen(port, () => {
   console.log(`chef API is running on port:${port}`);
 });
